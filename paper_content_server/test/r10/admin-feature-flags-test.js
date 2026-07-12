416302d5717581d7d34916ea85d925fbab3c7804
phoebus <phoebus@local>
2026-07-13 00:04:41 +0200

    chore(integration): wire mqtt:port-test and csrf/translation tests into CI and admin:test; fix r10 feature-flags unit test to pass options

--- paper_content_server/test/r10/admin-feature-flags-test.js
+++ paper_content_server/test/r10/admin-feature-flags-test.js
@@ -2,7 +2,7 @@
 var path=require('path');var ROOT=path.join(__dirname,'..','..');var ec=0,pass=0,fail=0;
 function t(n,o,d){console.log((o?'PASS':'FAIL')+' '+n+(d?': '+d:''));if(o)pass++;else{ec=1;fail++}}
 var FV=require(path.join(ROOT,'src','admin','feature-flag-view'));
-var flags=FV.getFeatureFlags();
+var flags=FV.getFeatureFlags({ newsPipeline: { /* mock */ } });
 t('FLAGS_EXIST',typeof flags==='object','');
 t('NEWS_PIPELINE',flags.newsPipeline.configured===true,'');
 t('ADMIN_READONLY',flags.adminReadOnly.enabled===true,'');
